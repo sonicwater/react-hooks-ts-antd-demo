5897e7e788ba7901f7ae313ee4d6868995cea3e3
sonic <sonicwater@126.com>
2020-09-14 13:04:46 +0800

    eject

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { HelmetComponent } from "components/helmet";
+import React,{ useState } from 'react'
 
+import './main.scss';
+import { HelmetComponent } from "components/helmet";
+import { Layout } from 'antd';
+const { Header, Content, Sider } = Layout;
 
 export default () => {
 
+  const [displayState, setDisplayState] = useState({
+    categorySiderDisplay: false,
+    cartSiderDisplay: false
+  });
+
   return (
-    <>
-      <HelmetComponent helmet='Ant Design' />
-    </>
+    <div>
+      <Layout className="container">
+        <Header className="header">
+          <HelmetComponent helmet='Ant Design' />
+        </Header>
+        <Layout>
+          <Sider
+              collapsible={true}
+              theme={"light"}
+              trigger={null}
+              collapsed={!displayState.categorySiderDisplay}
+          >
+            1
+          </Sider>
+          <Content className="content">
+            2
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
   )
   
 }
